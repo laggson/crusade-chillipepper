143ddb22499268dc913341e3e3068dcf4271f9f8
Laggson <m.estrich98@gmail.com>
2016-04-26 12:52:29 +0200

    Initial Push - Added NuGets - Prepared MainWindow - Implemented Resources in App.xaml

---
+++ FWAdministraion/FWA.Gui/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+
+namespace FWA.Gui
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Version ver = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            this.Title = "FWAdministration v" + ver.Major + "." + ver.Minor + "." + ver.Revision;
+        }
+    }
+}
